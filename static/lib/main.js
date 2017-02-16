b6fdf8c7c7fba41960446b9d352f315d0b56d153
atinosun <atinosun@yeah.net>
2017-02-16 17:28:55 +0800

    修正之前写错的插件api地址。

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 $(document).ready(function() {
-	$.get(RELATIVE_PATH+"/api/plugins/google-analytics-multiple").done(function(config){
+	$.get(RELATIVE_PATH+"/api/plugins/google-analytics").done(function(config){
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -22,7 +22,7 @@ $(document).ready(function() {
 
     }).fail(function() {
         if (window.console) {
-            console.warn("[plugin/google-analytics-multiple]Please check your Google Analytics Track ID in the plugin.It might be wrong.");
+            console.warn("[plugin/google-analytics-ccmt]Please check your Google Analytics Track ID in the plugin.It might be wrong.");
         };
 
     });
